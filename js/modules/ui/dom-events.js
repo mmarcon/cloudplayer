fbde68a0ccdd6dbad178f61bdddc1438268ac632
Massimiliano Marcon <massimiliano.marcon@nokia.com>
2013-07-04 19:26:23 +0200

    Pulled in jQuery.
    Makes code much cleaner.

--- js/modules/ui/dom-events.js
+++ js/modules/ui/dom-events.js
@@ -4,88 +4,86 @@ define(function(require) {
 
     //@TODO: add touch events
 
-    var DOM = require('modules/ui/dom'),
-        Controller = require('modules/controller'),
+    var Controller = require('modules/controller'),
         Events = require('modules/events'),
         Queue = require('modules/queue'),
-        $ = DOM.$,
+        Template = require('modules/ui/template'),
+        $ = require('jquery'),
         dispatcher = Events.getDispatcher();
 
     function bind(){
-        $('.search form')[0].on('submit', function(e){
-            //Let's just handle everything JS side
-            e.preventDefault();
-
-            //Basic query validation
-            var searchTerms = $('#searchTerms')[0].value;
-            if(searchTerms && searchTerms.length > 2) {
-                Controller.search(searchTerms);
-            }
-        });
-        $('.search .results')[0].on('click', function(e){
-            e.preventDefault();
-            var target = e.target,
-                className = target.className;
-
-            if(target && /enqueue/.test(className) && !(/disabled/.test(className))) {
-                target.className += ' disabled';
-                Queue.enqueue(DOM.data(target.parentNode, 'track'));
-            }
-        });
-        $('.slider')[0].on('click', function(e){
-            e.preventDefault();
-            var target = e.target,
-                className = target.className;
-            if(target && /slide-control/.test(className)) {
-                if(this.className === 'slider') {
-                    this.className = 'slider slide';
-                } else {
-                    this.className = 'slider';
-                }
-            } else if(target && /toggleplay/.test(className)) {
-                var track = Queue.get(0);
-                console.log(track);
-                Controller.play(track.id);
-            }
-        });
+        $('.search form').on('submit', searchFormSubmitted);
+        $('.search .results').on('touchstart click', '.enqueue', trackEnqueueClicked);
+        $('.slide-control').on('touchstart click', sliderActivated);
+        $('.toggleplay').on('touchstart click', playToggled);
         observe();
+    }
+
+    function searchFormSubmitted(e) {
+        //Let's just handle everything JS side
+        e.preventDefault();
+
+        var searchBox = $('#searchTerms');
+        //Should hide the keyboard on iOS
+        searchBox.blur();
+        //Basic query validation
+        var searchTerms = searchBox.val();
+        if(searchTerms && searchTerms.length > 1) {
+            Controller.search(searchTerms);
+        }
+    }
+
+    function trackEnqueueClicked(e) {
+        e.preventDefault();
+        Queue.enqueue($(this).parent().data('track'));
+    }
+
+    function sliderActivated(e) {
+        e.preventDefault();
+        $('.slider').toggleClass('slide');
+    }
+
+    function playToggled(e) {
+        e.preventDefault();
+        var track = Queue.get(0);
+        console.log(track);
+        Controller.play(track.id);
     }
 
     function observe(){
         dispatcher.on(Events.SEARCH_RESULTS, function(results){
-            var searchResultsList = $('.search .results')[0];
-            DOM.empty(searchResultsList);
+            var searchResultsList = $('.search .results');
+            searchResultsList.empty();
 
             if(!results) {
                 return;
             }
             results.forEach(function(res){
-                var li;
+                var li = $('<li>');
                 //If the track does not have an artwork then use default image
                 res.artwork_url = res.artwork_url || 'img/default-artwork.png';
-                li = document.createElement('li');
                 //Attach the original search result to the DOM node
                 //so we can easy retrieve it when it is time to enqueue
                 //this track
-                DOM.data(li, 'track', res);
-                li.innerHTML = DOM.tmpl('searchResult', res);
-                searchResultsList.appendChild(li);
+                li.data('track', res);
+                li.html(Template.parse('searchResult', res));
+                searchResultsList.append(li);
             });
         });
         dispatcher.on(Events.QUEUE_CHANGED, function(queue){
-            var playlist = $('.player .playlist')[0];
-            DOM.empty(playlist);
+            var playlist = $('.player .playlist');
+            playlist.empty();
             if(!queue) {
                 return;
             }
             queue.forEach(function(item){
-                var li = document.createElement('li');
+                var li = $('<li>');
                 //Attach the original item to the DOM node
                 //so we can easy retrieve it when it is time to enqueue
                 //this track
-                DOM.data(li, 'track', item);
-                li.innerHTML = DOM.tmpl('playlistItem', item);
-                playlist.appendChild(li);
+                li.data('track', item);
+                li.html(Template.parse('playlistItem', item));
+                playlist.append(li);
             });
         });
     }
